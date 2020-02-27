c63afc621c43ac509f1645491003567449409fa4
603156 <jeffreyfu@C02Y70APJGH7.fios-router.home>
2020-02-26 19:30:23 -0500

    added more icons

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 
+import CodeIcon from 'icons/code.svg';
+import InfoIcon from 'icons/info.svg';
+import ProjectIcon from 'icons/project.svg';
+import CogsIcon from 'icons/cogs.svg';
+import EnvelopIcon from 'icons/envelop.svg';
+
 class Nav extends Component {
     constructor(props) {
         super(props);
@@ -31,19 +37,24 @@ class Nav extends Component {
         return (
             <nav className={`nav ${this.state.scrolled && 'nav--scrolled'}`}>
                 <h1 className="nav-logo">
+                    <CodeIcon />
                     <AnchorLink href="#">{this.props.siteTitle}</AnchorLink>
                 </h1>
                 <ul className="nav-list">
                     <li className="nav-list__item">
+                        <InfoIcon />
                         <AnchorLink href="#section-about">About</AnchorLink>
                     </li>
                     <li className="nav-list__item">
+                        <ProjectIcon />
                         <AnchorLink href="#section-projects">Projects</AnchorLink>
                     </li>
                     <li className="nav-list__item">
+                        <CogsIcon />
                         <AnchorLink href="#section-uses">Uses</AnchorLink>
                     </li>
                     <li className="nav-list__item">
+                        <EnvelopIcon />
                         <AnchorLink href="#section-contact">Contact</AnchorLink>
                     </li>
                 </ul>
