356bf57321bd47caaad0e9c5c7ce7d3c9a3def64
Jeffrey Fu <fujeffrey1@gmail.com>
2020-02-27 19:00:21 -0500

    wip

--- src/components/about.js
+++ src/components/about.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
-const About = () => <section id="section-about">About</section>;
+import LocationIcon from 'icons/location.svg';
+import GraduationIcon from 'icons/graduation.svg';
+import OfficeIcon from 'icons/office.svg';
+
+const About = () => (
+    // TODO: Two column layout
+    <section id="section-about">
+        <LocationIcon />
+        <GraduationIcon />
+        <OfficeIcon />
+        About
+    </section>
+);
 
 export default About;
