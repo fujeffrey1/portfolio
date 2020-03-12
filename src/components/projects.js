6c593276b1029c0fb6b387c20df56615d59cf453
603156 <jeffreyfu@C02Y70APJGH7.fios-router.home>
2020-03-11 20:27:14 -0400

    project wip

--- src/components/projects.js
+++ src/components/projects.js
@@ -3,11 +3,78 @@ import React from 'react';
 import Image from './image';
 import ProjectIcon from 'icons/project.svg';
 
-const LINKS = {
-    huddle: 'https://github.com/fujeffrey1/huddle',
-    sickfits: 'https://github.com/fujeffrey1/sickfits',
-    patter: 'https://github.com/fujeffrey1/twitter',
-    hemlock: 'https://github.com/fujeffrey1/hemlock'
+const PROJECTS = {
+    huddle: {
+        links: {
+            github: 'https://github.com/fujeffrey1/huddle',
+            demo: 'https://powerful-fjord-22140.herokuapp.com/'
+        },
+        text: `is an anonymous messaging app where users can communicate privately. This is especially
+        important in today's world where corporations are always gathering personal data. Users are able
+        to create or join an unlimited amount of rooms with aliases of their choice. They will then be
+        able to chat with features like typing indicators, emojis, sharing room links, and timestamps.
+        When leaving rooms, messages will be unrecoverable.`,
+        uses: ['Svelte', 'Sapper', 'Polka', 'Socket IO']
+    },
+    sickfits: {
+        links: {
+            github: 'https://github.com/fujeffrey1/sickfits'
+        },
+        text: `is an e-commerce site where users are able to buy and sell products after signup. Similar to platforms like Amazon and eBay, it includes many features like authentication, emailing, credit card checkout, image uploading, and more. What makes this project different is the unique interface as well as developer experience. `,
+        uses: ['Next.js', 'Apollo', 'Prisma', 'Stripe']
+    },
+    patter: {
+        links: {
+            github: 'https://github.com/fujeffrey1/twitter'
+        },
+        text: `is an anonymous messaging app where users can communicate privately. This is especially
+        important in today's world where corporations are always gathering personal data. Users are able
+        to create or join an unlimited amount of rooms with aliases of their choice. They will then be
+        able to chat with features like typing indicators, emojis, sharing room links, and timestamps.
+        When leaving rooms, messages will be unrecoverable.`,
+        uses: ['React', 'Redux', 'Node.js', 'MongoDB']
+    },
+    hemlock: {
+        links: {
+            github: 'https://github.com/fujeffrey1/hemlock'
+        },
+        text: `is an anonymous messaging app where users can communicate privately. This is especially
+        important in today's world where corporations are always gathering personal data. Users are able
+        to create or join an unlimited amount of rooms with aliases of their choice. They will then be
+        able to chat with features like typing indicators, emojis, sharing room links, and timestamps.
+        When leaving rooms, messages will be unrecoverable.`,
+        uses: ['Spring', 'Bootstrap', 'Thymeleaf', 'MySQL']
+    }
+};
+
+const renderProjects = () => {
+    return Object.entries(PROJECTS).map(([project, { links, text, uses }]) => (
+        <div className="project" key={project}>
+            <a target="_blank" rel="noopener noreferrer" href={links.github}>
+                <figure className="project__figure">
+                    <Image filename={`${project}.png`} alt={project} className="project__img" />
+                </figure>
+            </a>
+            <div className="project__text">
+                <h3>{project}</h3>
+                <p>{text}</p>
+                <span className="project__links">
+                    {Object.entries(links).map(([name, url]) => (
+                        <a key={name} target="_blank" rel="noopener noreferrer" href={url} className="btn">
+                            {name}
+                        </a>
+                    ))}
+                </span>
+            </div>
+            <div className="project__uses">
+                {uses.map(use => (
+                    <span key={use} className="project__bubble">
+                        {use}
+                    </span>
+                ))}
+            </div>
+        </div>
+    ));
 };
 
 const Projects = () => (
@@ -16,60 +83,7 @@ const Projects = () => (
             <ProjectIcon />
             Projects
         </h2>
-        <div className="section-projects__content">
-            <div className="project">
-                <a target="_blank" rel="noopener noreferrer" href={LINKS.huddle}>
-                    <figure className="project__figure">
-                        <Image filename="huddle.png" alt="huddle" className="project__img" />
-                    </figure>
-                </a>
-                <div className="project__text">
-                    <h3>Huddle</h3>
-                    <p>
-                        is an anonymous messaging app where users can communicate in private. This is especially
-                        important in today's world where corporations are always gathering personal data. Users are able
-                        to create or join any room with an alias.
-                    </p>
-                </div>
-                <div className="project__uses"></div>
-            </div>
-            <div className="project">
-                <a target="_blank" rel="noopener noreferrer" href={LINKS.sickfits}>
-                    <figure className="project__figure">
-                        <Image filename="sickfits.png" alt="sickfits" className="project__img" />
-                    </figure>
-                </a>
-                <div className="project__text">
-                    <h3>Sickfits</h3>
-                    <p></p>
-                </div>
-                <div className="project__uses"></div>
-            </div>
-            <div className="project">
-                <a target="_blank" rel="noopener noreferrer" href={LINKS.patter}>
-                    <figure className="project__figure">
-                        <Image filename="patter.png" alt="patter" className="project__img" />
-                    </figure>
-                </a>
-                <div className="project__text">
-                    <h3>Patter</h3>
-                    <p></p>
-                </div>
-                <div className="project__uses"></div>
-            </div>
-            <div className="project">
-                <a target="_blank" rel="noopener noreferrer" href={LINKS.hemlock}>
-                    <figure className="project__figure">
-                        <Image filename="hemlock.png" alt="hemlock" className="project__img" />
-                    </figure>
-                </a>
-                <div className="project__text">
-                    <h3>Hemlock</h3>
-                    <p></p>
-                </div>
-                <div className="project__uses"></div>
-            </div>
-        </div>
+        <div className="section-projects__content">{renderProjects()}</div>
     </section>
 );
 
